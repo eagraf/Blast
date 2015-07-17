834bb1745a0cc04cef48186b8865c2e80a6ad635
eagraf <graf.ethan@gmail.com>
2015-07-16 23:10:28 -0400

    Fixed toolbar and some other stuff.

--- app/src/main/java/oompa/loompa/blast/MainActivity.java
+++ app/src/main/java/oompa/loompa/blast/MainActivity.java
@@ -3,69 +3,63 @@ package oompa.loompa.blast;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mDrawerView;
 
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
 
     private NotificationListFragment notificationListFragment;
+    private GroupListFragment groupListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Set up the toolbar
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+
         //Enable ActionBar app icon to behave as action to toggle nav drawer
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        mToolbar.setNavigationIcon(R.drawable.ic_launcher);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
+        //Initialize some navigation drawer stuff.
         mDrawerLayout = (DrawerLayout) findViewById(R.id.navigation_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
-
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        //Create the drawer listener(listens for events involving the navigation drawer being opened and closed).
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerView = (NavigationView) findViewById(R.id.navigation_view);
+        mDrawerView.setNavigationItemSelectedListener(this);
 
         fragmentManager = getFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
 
         notificationListFragment = new NotificationListFragment();
+        groupListFragment = new GroupListFragment();
         fragmentTransaction.add(R.id.navigation_layout, notificationListFragment);
         fragmentTransaction.commit();
-    }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
@@ -74,18 +68,37 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case R.id.action_settings:
+                return true;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-        //For the hamburger icon.
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    //Handle click events on navigation drawer items.
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        System.out.println("HIIIIIIIIIIIIII");
+        menuItem.setChecked(true);
+        switch (menuItem.getItemId()) {
+            //Go to the notification list.
+            case R.id.notification_list_item:
+                fragmentTransaction.replace(R.id.navigation_layout, notificationListFragment);
+                fragmentTransaction.commit();
+                return true;
+            //Go to the group list.
+            case R.id.group_list_item:
+                fragmentTransaction.replace(R.id.navigation_layout, groupListFragment);
+                fragmentTransaction.commit();
+                return true;
+            default:
+                return true;
+        }
+    }
+
 
     //Remove a notification from the notification list view.
     public void removeNotification(View v) {
